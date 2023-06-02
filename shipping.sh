9bf251a3559b3cf0ff21b2fe1e009e4873899ab1
devopsengineer <rohitnone1998@gmail.com>
2023-06-02 12:30:15 +0530

    add roboshop nginx

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,34 @@
-echo -e "\e[33m Install Redis Repos \e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>/tmp/roboshop.log
+echo -e "\e[33m Install maven \e[0m"
+yum install maven -y  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Enable Redis 6 Version \e[0m"
-yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+echo  -e "\e[33m Add application user \e[0m"
+useradd roboshop   &>>/tmp/roboshop.log
 
-echo -e "\e[33m Install Redis \e[0m"
-yum install redis -y &>>/tmp/roboshop.log
+echo -e "\e[33m Create an application directory\e[0m"
+rm -rf /app   &>>/tmp/roboshop.log
+mkdir /app
 
-echo -e "\e[33m Update Redis Listen address \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+echo -e "\e[33m Download Application content \e[0m"
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip  &>>/tmp/roboshop.log
+cd /app
 
-echo -e "\e[33m Start Redis Service \e[0m"
-systemctl enable redis  &>>/tmp/roboshop.log
-systemctl restart redis  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Extract Application content \e[0m"
+unzip /tmp/shipping.zip  &>>/tmp/roboshop.log
+
+
+echo -e "\e[33m Download Depedencies \e[0m"
+
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
+
+echo -e "\e[33m Install Mysql client \e[0m"
+yum install mysql -y  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Load Mysql Schema \e[0m"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>/tmp/roboshop.log
+
+echo -e "\e[33m Start Shipping Service \e[0m"
+systemctl daemon-reload     &>>/tmp/roboshop.log
+systemctl enable shipping  &>>/tmp/roboshop.log
+systemctl restart shipping  &>>/tmp/roboshop.log
